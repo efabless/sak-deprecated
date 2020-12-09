72993c5310c7bdec0f4ffd4939f4b746fd758a16
agorararmard <aagouhar@efabless.com>
2020-12-09 22:35:18 +0200

    update calibreSummary.py to:
        - Added missing license
        - Added category column based on parent folder name.
        - Added rule_letter column to filter r/k/s
        - Auto creation for the output directory if it doesn't exist.
        - Made the script executable.

--- calibreSummary.py
+++ calibreSummary.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# Copyright 2020 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import re
 import os
@@ -15,17 +30,21 @@ parser.add_argument("--outputDirectory", "-o", required=False,
                     help="Output Directory")
 
 args = parser.parse_args()
-summaryFolder = args.targetPath
+summaryFolder = os.path.abspath(args.targetPath)
 outputDirectory = args.outputDirectory
 if outputDirectory is None:
     outputDirectory = summaryFolder
+
+if not os.path.exists(outputDirectory):
+    os.makedirs(outputDirectory)
+
 waiveList=[]
 if args.waivableList is not None:
     waivableListFileOpener = open(args.waivableList,"r")
     waiveList = waivableListFileOpener.read().split()
     waivableListFileOpener.close()
 
-header = "RULE,Waivable,COUNT 1, COUNT 2\n"
+header = "RULE,Waivable,rule_letter,category,COUNT 1, COUNT 2\n"
 
 def getListOfFiles(dirName):
     # create a list of file and sub directories
@@ -48,7 +67,8 @@ def extractSummary(summaryFile):
     summaryFileOpener = open(summaryFile,"r")
     summaryContent = summaryFileOpener.read().split("\n")
     summaryFileOpener.close()
-    outputFile = outputDirectory+'/'+os.path.basename(summaryFile)+'.csv'
+    rule_category=os.path.basename(os.path.dirname(summaryFile))
+    outputFile = outputDirectory+'/'+rule_category+'.'+os.path.basename(summaryFile)+'.csv'
 
     splitter = "----------------------------------------------------------------------------------"
     ruleCheckString = "RULECHECK RESULTS STATISTICS"
@@ -57,12 +77,12 @@ def extractSummary(summaryFile):
 
     outputFileOpener = open(outputFile, "w+")
     outputFileOpener.write(header)
-    outputFileOpener.close()            
+    outputFileOpener.close()
 
     for line in summaryContent:
         if line.find(ruleCheckString) != -1:
             beginFlag = True
-        
+
         if beginFlag:
             if line == splitter:
                 break
@@ -71,22 +91,23 @@ def extractSummary(summaryFile):
 
             if len(rule) == 1:
                 ruleName = rule[0][0]
+                rk=ruleName[0]
                 waivable= 'no'
                 if ruleName in waiveList:
                     waivable='yes'
                 outputFileOpener = open(outputFile, "a+")
-                outputFileOpener.write(str(",".join((ruleName, waivable,rule[0][1],rule[0][2])))+'\n')
-                outputFileOpener.close()            
-                
-                mainOutputFileOpener = open(mainOutputFile, "a+")
-                mainOutputFileOpener.write(str(",".join((ruleName, waivable,rule[0][1],rule[0][2])))+'\n')
-                mainOutputFileOpener.close()  
+                outputFileOpener.write(str(",".join((ruleName, waivable,rk,rule_category,rule[0][1],rule[0][2])))+'\n')
+                outputFileOpener.close()
 
-mainOutputFile = outputDirectory+'/'+os.path.dirname(summaryFolder+'/')+'_merged.csv'
+                mainOutputFileOpener = open(mainOutputFile, "a+")
+                mainOutputFileOpener.write(str(",".join((ruleName, waivable,rk,rule_category,rule[0][1],rule[0][2])))+'\n')
+                mainOutputFileOpener.close()
+
+mainOutputFile = outputDirectory+'/'+os.path.basename(summaryFolder)+'_merged.csv'
 
 mainOutputFileOpener = open(mainOutputFile, "w+")
 mainOutputFileOpener.write(header)
-mainOutputFileOpener.close()            
+mainOutputFileOpener.close()
 
 files = getListOfFiles(summaryFolder)
 for f in files:
